901a1e4663abc63d340920eb34dfcf5951ac6aed
Riku <134082872+rigozu9@users.noreply.github.com>
2024-02-06 12:00:16 +0200

    8.2 done

--- index.js
+++ index.js
@@ -98,16 +98,25 @@ let books = [
 */
 
 const typeDefs = `
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-  }
+    type Book {
+        title: String!
+        published: Int!
+        author: String!
+        genres: [String!]!
+        id: ID! 
+    }
+    type Query {
+        bookCount: Int!
+        authorCount: Int!
+        allBooks: [Book!]!
+    }
 `
 
 const resolvers = {
     Query: {
         bookCount: () => books.length,
         authorCount: () => authors.length,
+        allBooks: () => books,
   }
 }
 
