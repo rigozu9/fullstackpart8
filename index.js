1ca11f0b92c36db66f899cf03542bd846818bd02
Riku <134082872+rigozu9@users.noreply.github.com>
2024-02-10 14:13:39 +0200

    8.7 done

--- index.js
+++ index.js
@@ -122,6 +122,10 @@ const typeDefs = `
         published: Int!
         genres: [String!]
       ): Book
+      editAuthor(
+        name: String!
+        setBornTo: Int!
+      ): Author
     }
 `
 
@@ -168,7 +172,17 @@ const resolvers = {
         books = books.concat(book)
     
         return book
-      }
+      },
+      editAuthor: (root, args) => {
+        const author = authors.find(a => a.name === args.name)
+        if (!author) {
+          return null
+        }
+    
+        const updatedAuthor = { ...author, born: args.setBornTo }
+        authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+        return updatedAuthor
+      } 
     }
 }
 
