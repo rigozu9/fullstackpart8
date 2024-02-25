0f43717955caeb50ace6f3d7f7085f9e4e9223a7
Riku <134082872+rigozu9@users.noreply.github.com>
2024-02-25 14:48:32 +0200

    8.9 done

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,9 +1,12 @@
-const Books = (props) => {
-  if (!props.show) {
-    return null
-  }
+import { ALL_BOOKS } from '../queries'
+import { useQuery } from '@apollo/client'
 
-  const books = []
+const Books = (props) => {
+  const { loading, error, data } = useQuery(ALL_BOOKS)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+  if (!props.show || !data) return null
 
   return (
     <div>
@@ -16,7 +19,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {data.allBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
