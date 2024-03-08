cbc6254513885b3d61a1ec8ee4e80fb498f87089
Riku <134082872+rigozu9@users.noreply.github.com>
2024-03-08 11:35:19 +0200

    8.15 done

--- index.js
+++ index.js
@@ -1,84 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
-const { v1: uuid } = require('uuid')
-
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
+const { GraphQLError } = require('graphql')
 
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
@@ -98,7 +20,6 @@ mongoose.connect(MONGODB_URI)
   .catch((error) => {
     console.log('error connection to MongoDB:', error.message)
   })
-
 
 const typeDefs = `
     type Book {
@@ -175,36 +96,60 @@ const resolvers = {
     },
     Mutation: {
       addBook: async (root, args) => {
-        let author = await Author.findOne({ name: args.author })
-    
-        if (!author) {
-          const authorArgs = { name: args.author, born: args.born }
-          author = new Author(authorArgs)
-          await author.save()
-        }
-
-        const book = new Book({
-          title: args.title,
-          published: args.published,
-          genres: args.genres,
-          author: author._id,
-        })
-    
-        await book.save()
-    
-        await book.populate('author')
+        try {
+          let author = await Author.findOne({ name: args.author })
+      
+          if (!author) {
+            const authorArgs = { name: args.author, born: args.born }
+            author = new Author(authorArgs)
+            await author.save()
+          }
   
-        return book
+          const book = new Book({
+            title: args.title,
+            published: args.published,
+            genres: args.genres,
+            author: author._id,
+          })
+      
+          await book.save()
+          await book.populate('author')
+    
+          return book
+        } catch (error) {
+          throw new GraphQLError('Error adding book: ' + error.message, {
+            extensions: {
+              code: 'DATABASE_ERROR',
+              invalidArgs: Object.keys(args),
+              errorMessage: error.message,
+            }
+          })
+        }
       },
       editAuthor: async (root, args) => {
-        const updatedAuthor = await Author.findOneAndUpdate(
-          { name: args.name },
-          { born: args.setBornTo },
-          { new: true }
-        )
-        return updatedAuthor
+          try {
+            const updatedAuthor = await Author.findOneAndUpdate(
+              { name: args.name },
+              { born: args.setBornTo },
+              { new: true, runValidators: true }
+            )
+    
+            if (!updatedAuthor) {
+              throw new Error('Author not found')
+            }
+    
+            return updatedAuthor
+          } catch (error) {
+            throw new GraphQLError('Error editing author: ', {
+              extensions: {
+                code: 'DATABASE_ERROR',
+                invalidArgs: Object.keys(args),
+                error
+              }
+        })
       }      
     }
+  },
 }
 
 const server = new ApolloServer({
