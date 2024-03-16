4e7ed4919137f16fd21324cfd0c60da78e62eb67
Riku <134082872+rigozu9@users.noreply.github.com>
2024-03-16 20:25:56 +0200

    8.25 done

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useApolloClient, useSubscription } from '@apollo/client'
-import { BOOK_ADDED } from './queries.js'
+import { BOOK_ADDED, ALL_BOOKS } from './queries.js'
 
 import Authors from './components/Authors'
 import Books from './components/Books'
@@ -8,28 +8,48 @@ import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
 import Recommendations from './components/Recommendations'
 
+export const updateCache = (cache, query, addedBook) => {
+  // helper that is used to eliminate saving same person twice
+  const uniqByName = (a) => {
+    let seen = new Set()
+    return a.filter((item) => {
+      let k = item.id
+      return seen.has(k) ? false : seen.add(k)
+    })
+  }
+
+  cache.updateQuery(query, ({ allBooks }) => {
+    console.log(allBooks);
+    return {
+      allBooks: uniqByName(allBooks.concat(addedBook)),
+    }
+  })
+}
+
 const App = () => {
   const [page, setPage] = useState('authors')
   const [token, setToken] = useState(null)
   const client = useApolloClient()
+
+  useSubscription(BOOK_ADDED, {
+    onData: ({ data }) => {
+      if (data?.data?.bookAdded) {
+        const addedBook = data?.data?.bookAdded
+        const title = data.data.bookAdded.title
+        const authorName = data.data.bookAdded.author.name
+        alert(`A new book "${title}" by ${authorName} added`)
+
+        updateCache(client.cache, { query: ALL_BOOKS, variables: {genre: ''} }, addedBook)
+      }
+    }
+  })
 
   const logout = () => {
     setToken(null)
     localStorage.clear()
     client.clearStore()
     setPage('authors')
-  }
-
-  useSubscription(BOOK_ADDED, {
-    onData: ({ data }) => {
-      if (data?.data?.bookAdded) {
-        const title = data.data.bookAdded.title
-        const authorName = data.data.bookAdded.author.name
-        alert(`A new book "${title}" by ${authorName} added`)
-      }
-    }
-  })
-  
+  }  
 
   if (!token) {
       return (
